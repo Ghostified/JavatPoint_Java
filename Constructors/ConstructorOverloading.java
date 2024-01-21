1a75627d25c02a830bbf526281c36f11969ef179
Allan Branson <bransonaallan@gmail.com>
2024-01-21 22:16:15 +0300

    Creating multiple constructors with multiple type of data

--- Constructors/ConstructorOverloading.java
+++ Constructors/ConstructorOverloading.java
@@ -8,6 +8,39 @@ package Constructors;
  * - number of parameter in the lists
  * = their types 
  */
+
+ //Example of constructor overloading
+ class Student5 {
+    int id;
+    String name;
+    int age;
+
+    //creating two arg constructor
+    Student5 (int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    //creating a three arg constructor
+    Student5 ( int i, String n , int a) {
+        id = i;
+        name = n;
+        age = a;
+    }
+
+    void display () {
+        System.out.println(id + " " + name + " " + age);
+    }
+ }
 public class ConstructorOverloading {
+
+    public static void main (String [] Args) {
+        Student5 s1 = new Student5(111, " James");
+        Student5 s2 = new Student5(222, "Brown", 025);
+
+        s1.display();
+        s2.display();
+
+    }
 
 }
