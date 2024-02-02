a5335e29d0a3314f1b618f9947b1564a701e4044
Allan Branson <bransonaallan@gmail.com>
2024-02-02 22:12:57 +0300

    instatiation,declation , declarion example in one line

---
+++ Arrays/Array2.java
@@ -0,0 +1,15 @@
+package Arrays;
+/*
+ * Declare, intantiate and initialize in one line an array
+ */
+public class Array2 {
+    public static void main (String[]Args) {
+
+        //instatiation, declation and initialization in one line
+        int a [] = {4,5,6,7}; 
+
+        //print 
+        for (int i= 0 ; i < a.length ; i++)
+        System.out.println(a[i]);
+    }
+}
