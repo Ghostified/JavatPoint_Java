265396a9a52322a2b247637b92e76aadb1dea2c4
Allan Branson <bransonaallan@gmail.com>
2024-02-11 18:08:13 +0300

    Sorting an array of strings in acscending and descending order

--- Arrays/BasicOperations/SortArrayInAlphabeticalOrder.java
+++ Arrays/BasicOperations/SortArrayInAlphabeticalOrder.java
@@ -9,8 +9,14 @@ public class SortArrayInAlphabeticalOrder {
         Arrays.sort(array, Collections.reverseOrder());
         System.out.println("Array sorted in descending order" + Arrays.toString(array));
 
+        //Sort arry in ascending order using the Arrays.sort () method
         Arrays.sort(array);
         System.out.println("Array sorted in ascending order: " + Arrays.toString(array));
+
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 
 }
