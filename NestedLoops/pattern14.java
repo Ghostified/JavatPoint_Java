65df247bb9558804454d9e8d0ffce3f8efdaf78a
Allan Branson <bransonaallan@gmail.com>
2024-02-21 23:04:48 +0300

    number pattern

---
+++ NestedLoops/pattern14.java
@@ -0,0 +1,16 @@
+package NestedLoops;
+
+public class pattern14 {
+    public static void main(String[] args) {
+
+    int rows = 7, i, j;
+    for ( i = rows; i >= 1;  i--)
+    {
+        for ( j = i; j >= 1; j--)
+        {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+}
+}
