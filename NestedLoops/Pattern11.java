390dbed86301d6b427effb674395723d00deddcb
Allan Branson <bransonaallan@gmail.com>
2024-02-21 22:47:26 +0300

    two halves of an arrow pattern

---
+++ NestedLoops/Pattern11.java
@@ -0,0 +1,28 @@
+package NestedLoops;
+
+public class Pattern11 {
+
+    public static void main(String[] args) {
+        int rows = 8;
+
+        //print upper half pattern
+        for (int i = 1; i <=rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+
+        //print lower half
+        for (int i = rows -1; i >= 1; i--) 
+        {
+            for (int j = 1; j <= i ; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
