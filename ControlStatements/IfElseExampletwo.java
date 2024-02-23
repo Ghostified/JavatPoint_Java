82cb1ff42bc698983c7f61fc76bc97bb1de32567
Allan Branson <bransonaallan@gmail.com>
2024-02-24 00:04:23 +0300

    the if-else statement evaluates a boolean expression

---
+++ ControlStatements/IfElseExampletwo.java
@@ -0,0 +1,12 @@
+package ControlStatements;
+
+public class IfElseExampletwo {
+    public static void main (String [] Args) {
+        int x = 10;
+        if (x < 20 ) {
+            System.out.println("This is an if statement");
+        } else {
+            System.out.println("This is an else statement");
+        }
+    }
+}
