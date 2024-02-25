2b7fe833318ffae128e5bd0231f334561134ae57
Allan Branson <bransonaallan@gmail.com>
2024-02-25 23:50:52 +0300

    Using the string compare ignore camel case to compare strings stored in an array list

---
+++ Strings/EqualsIgnoreCaseMethod/Example2.java
@@ -0,0 +1,28 @@
+package Strings.EqualsIgnoreCaseMethod;
+
+import java.util.*;
+
+/*
+ * Testing string equality with other strings
+ */
+public class Example2 {
+
+    public static void main(String[] args) {
+        
+        String s1 = "Harry Potter";
+        ArrayList <String> list = new ArrayList<> ();
+        list.add("Ron");
+        list.add("Weasley");
+        list.add("harry potter");
+        list.add("Hermione Granger");
+
+        for (String str : list) {
+            if (str.equalsIgnoreCase(s1)){
+                System.out.println("Harry potter is present");
+            // } else {
+            //     System.out.println("Harry potter is absent");
+             }
+        }
+
+    }
+}
