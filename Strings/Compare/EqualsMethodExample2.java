1a921e075547d122288f64e95716926948e6cd67
Allan Branson <bransonaallan@gmail.com>
2024-02-25 22:34:00 +0300

    String comaprison of objects in a list using the equals() method

--- Strings/Compare/EqualsMethodExample2.java
+++ Strings/Compare/EqualsMethodExample2.java
@@ -5,7 +5,7 @@ public class EqualsMethodExample2 {
     public static void main(String[] args) {
         String s1 = "Harry";
         String s2 = "Harry";
-        String s3 = "Harry Potter";
+        String s3 = "Harry Potter ";
 
         System.out.println(s1.equals(s2)); // true
         if (s1.equals(s3)) {
