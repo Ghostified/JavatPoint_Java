b08c921ef5bd9d7c32b721a5c1573040e6de570c
Allan Branson <bransonaallan@gmail.com>
2024-02-27 00:08:44 +0300

    The method indexOf (int char , int fromIndex) returns the index of the first character that occurs after the given fromIndex

--- Strings/indexOfMethod/Example5.java
+++ Strings/indexOfMethod/Example5.java
@@ -6,7 +6,7 @@ public class Example5 {
         String s1 = "This is indexOf method";
 
         //passing char and index from
-        int index = s1.indexOf('e', 12); //returns index of this char
-        System.out.println("The index of the char 'e'  is:  " + index);
+        int ind = s1.indexOf('e', 12); //returns index of this char
+        System.out.println("The index of the char 'e'  is:  " + ind);
     }
 }
