07fb584420a08e65b998718b37e1b0dd44396480
Allan Branson <bransonaallan@gmail.com>
2024-02-26 15:55:46 +0300

    using the string formart() method to convert strings into doiffrent data types and text formartting e.g padding, and text width

--- Strings/FormartMethod/Example3.java
+++ Strings/FormartMethod/Example3.java
@@ -6,10 +6,10 @@ public class Example3 {
 
     public static void main (String [] Args) {
         String s1 = String.format("%d",101 );
-        String s2 = String.format("|%10d|",101);
-        String s3 = String.format("|%-10d|",101 );
+        String s2 = String.format("|%10d|",101); //Specify lenghth of an integer
+        String s3 = String.format("|%-10d|",101 ); //left justifying within the specified width
         String s4 = String.format("|% d|",101);
-        String s5 = String.format("|%010d|",101);
+        String s5 = String.format("|%010d|",101); //filling with zeros
 
         System.out.println(s1);
         System.out.println(s2);
