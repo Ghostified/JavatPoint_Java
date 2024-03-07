be4e71818c59125a85e78bf26eb2337e130ae84a
Allan Branson <bransonaallan@gmail.com>
2024-03-07 22:57:16 +0300

    trim method used to detect white spaces with an if statement

--- Strings/TrimMethod.java
+++ Strings/TrimMethod.java
@@ -15,6 +15,9 @@ public class TrimMethod {
 
     Example52 object1 = new Example52();
     object1.trimMethod2();
+
+    Example53 object2 = new Example53();
+    object2.trimWhiteSpaces();
    }
 }
 class Example51 {
@@ -36,5 +39,30 @@ class Example52 {
         String str = stringOne.trim();
         System.out.println(str.length());
         //System.out.println(str);
+    }
+}
+//Trim method used to remove white spaces
+class Example53 {
+    public static void trimWhiteSpaces () {
+        TrimMethod instance = new TrimMethod();
+        String stringOne = instance.getStringOne();
+        if ((stringOne.trim()).length() > 0)
+        {
+            System.out.println("The string contains characters other than white spaces \n");
+        }
+        else 
+        {
+            System.out.println("The string contains only white spaces ");
+        }
+
+        stringOne = "  ";
+        if ((stringOne.trim()).length() > 0)
+        {
+            System.out.println("The string contains characters other than white spcaes \n");
+        }
+        else 
+        {
+            System.out.println("The string contains only white spaces ");
+        }
     }
 }
