8d32995b0e6cbd731d7f46eb05069cb04585b9ee
Allan Branson <bransonaallan@gmail.com>
2024-03-08 16:14:48 +0300

    regex classes

---
+++ Regex/Regex.java
@@ -0,0 +1,48 @@
+package Regex;
+/*
+ * Regex = Regular expresssion to define a pattern for searching or manipulating strings
+ * Used to define constraints on emails or passwords
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static void main(String[] args) {
+        Example1 obj = new Example1();
+        obj.regex1();
+
+        Example2 obj2 = new Example2();
+        obj2.regex2();
+    }
+}
+class Example1 {
+    public static void regex1 () {
+
+        //1st Way
+        Pattern p = Pattern.compile(".s"); //represents a single char
+        Matcher m = p.matcher("as");
+        boolean b = m.matches();
+
+        //2nd way
+        boolean b2 = Pattern.compile(".s").matcher("as").matches();
+
+        //third way 
+        boolean b3 = Pattern.matches(".s", "as");
+
+        System.out.println(b + " " + b2 + " " + b3);
+    }
+}
+
+class Example2 {
+    public static void regex2 () {
+
+        //The dod (s) represents a single character  
+        System.out.println(Pattern.matches(".s","as")); //true  (second char is s)
+        System.out.println(Pattern.matches(".s","mk")); //false - second char is not s
+        System.out.println(Pattern.matches(".s","mst")); //false - has more than two chars
+        System.out.println(Pattern.matches(".s","amms")); //false - has more than two chars
+        System.out.println(Pattern.matches("..s","mas")); //true - Third char is s
+    }
+}
