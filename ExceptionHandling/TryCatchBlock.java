be398b895d6734f5410f210bf83f4ed39350402d
Allan Branson <bransonaallan@gmail.com>
2024-03-11 14:53:29 +0300

    try catch block

--- ExceptionHandling/TryCatchBlock.java
+++ ExceptionHandling/TryCatchBlock.java
@@ -6,6 +6,12 @@ package ExceptionHandling;
 public class TryCatchBlock {
 
     public static void main(String[] args) {
+
+        Example2 obj = new Example2();
+        obj.customCatchBlock();
+
+        Example3 obj1 = new Example3();
+        obj1.resolveException();
         
     }
 }
@@ -20,3 +26,37 @@ class Example1{
         System.out.println("rest of the code....");
     }
 }
+ //print a custom exception message
+ class Example2 {
+    public static void customCatchBlock () {
+
+        try {
+            int data = 500/0;
+
+        }
+        catch (Exception e) {
+            System.out.println("Amathematical error -- Thios is custom");
+        }
+
+        System.out.println("Rest of the code");
+    }
+ }
+ //resloving an exception
+  class Example3  {
+    public static void resolveException () {
+        int i = 50; 
+        int j = 0;
+        int data;
+        try {
+
+            //may throw an exception
+            data = i/j;
+        }
+        //handling exception
+        catch (Exception e) 
+        {
+            //resolving
+            System.out.println(i/(j+2));
+        }
+    }
+  }
