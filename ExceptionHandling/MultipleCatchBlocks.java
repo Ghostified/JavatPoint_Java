69b2eb63550503be9311acdd30a26f7189b10157
Allan Branson <bransonaallan@gmail.com>
2024-03-11 23:57:05 +0300

    multiple catch blocks

--- ExceptionHandling/MultipleCatchBlocks.java
+++ ExceptionHandling/MultipleCatchBlocks.java
@@ -8,6 +8,15 @@ public class MultipleCatchBlocks {
     public static void main(String[] args) {
         Example10 obj = new Example10();
         obj.multipleCatchBlocks();
+
+        Example11 obj2 = new Example11();
+        obj2.multipleCatchBlock2();
+
+        Example12 obj3 = new Example12();
+        obj3.multipleCatchBlock3();
+
+        Example13 obj4 = new Example13();
+        obj4.multipleCatchBlock4();
     }
 
 }
@@ -31,5 +40,61 @@ class Example10 {
             System.out.println("Parent exception Occurs");
         }
         System.out.println("Rest of the code");
+    }
+}
+class Example11 {
+    public static void multipleCatchBlock2 () {
+        try {
+            int a [] = new int [5];
+            a[5] = 30/0;
+            System.out.println(a[10]);
+        }
+
+        catch (ArithmeticException e) 
+        {
+            System.out.println("Arithmetic Exception occurs");
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("ArrayOutOfBounds Exception occurs");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Parent Exception occurs");
+        }
+        System.out.println("rest of the code");
+    }
+}
+class Example12 {
+    public static void multipleCatchBlock3 (){
+        try {
+            String s = null;
+            System.out.println(s.length());
+        }
+        catch (ArithmeticException e) 
+        {
+            System.out.println("Arithmetic Exception occurs");
+        } 
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Parent Exception occurs");
+        }
+        System.out.println("rest of the code");
+    }
+}
+class Example13  {
+    public static void multipleCatchBlock4 () {
+        try {
+            int a [] = new int [5];
+            a[5] = 30/0;
+        }
+        catch (Exception e){System.out.println("Common task completed");}
+        catch(ArithmeticException e) {System.out.println("task 1 completed");}
+        catch(ArrayIndexOutOfBoundsException e) {System.out.println("task 2 completed");}
+        System.out.println("rest of the code");
     }
 }
