bf5362313acabeec950626f4fff1f8538f6e7a0b
Allan Branson <bransonaallan@gmail.com>
2024-03-12 23:04:46 +0300

    finally block is always executed , but if the exception is not handled properly, the program terminate sarbnormally

--- ExceptionHandling/FinallyBlock.java
+++ ExceptionHandling/FinallyBlock.java
@@ -10,6 +10,12 @@ public class FinallyBlock {
     public static void main(String[] args) {
         Example22 obj = new Example22();
         obj.finallyBlock();
+
+        Example23 obj1 = new Example23();
+        obj1.finallyBlock1();
+
+        Example24 obj2 = new Example24();
+        obj2.finallyBlock3();
     }
 }
 
@@ -36,3 +42,46 @@ class Example22{
         System.out.println("Rest of the code ");
     }
 }
+
+//when an exception occurs but the exception is not handled by the catch block
+//the program exits arbnormally, note from line 61 is not executed 
+class Example23 {
+    public static void finallyBlock1 (){
+        try {
+            System.out.println("Inside the try block");
+            int array [] = {1,2,3,4};
+            System.out.println(array[5]);
+        }
+        catch (NullPointerException e) {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Finally block always executed");
+        }
+        System.out.println("Rest of the code ");
+    }
+}
+
+//when exception is handled in the try block
+//finally is always executed 
+class Example24 {
+    public static void finallyBlock3 () {
+        try {
+            System.out.println("Inside the try block ");
+
+            int b = 34/0;
+            System.out.println(b);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Exception handled");
+            System.out.println(e);
+        }
+
+        finally {
+            System.out.println("Fibnally always executed");
+        }
+        System.out.println("Rest of the code ");
+    }
+}
+
