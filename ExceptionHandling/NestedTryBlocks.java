96f6188ba4ea56099b2ed11e7e1fbac4d74b16ff
Allan Branson <bransonaallan@gmail.com>
2024-03-12 22:14:27 +0300

    nested try blocks

--- ExceptionHandling/NestedTryBlocks.java
+++ ExceptionHandling/NestedTryBlocks.java
@@ -6,6 +6,9 @@ public class NestedTryBlocks {
     public static void main(String[] args) {
         Example20 obj = new Example20();
         obj.tryblock();
+
+        Example21 obj1 = new Example21();
+        obj1.nesting();
     }
 
 }
@@ -57,5 +60,57 @@ class Example20{
         System.out.print("normal flow");
 
 
+    }
+}
+
+class Example21  {
+    public static void nesting (){
+
+        //outer try block (main) 
+        try
+         {
+
+            //inner try block 1
+            try 
+            {
+
+
+                //innner try block 2
+                try 
+                {
+                    int arr [] = {1,2,3,4};
+                    //printing array elements out of bounds
+                    System.out.println(arr[10]);
+                }
+
+                //handles arithmetic exception
+                catch (ArithmeticException e)
+                {
+                    System.out.println("Arithmetic exception");
+                    System.out.println("Inner try block 1");
+                }
+            
+            
+            }
+
+            //to handle arithmetic exception
+            catch(ArithmeticException e)
+            {
+                System.out.println("Arithmetic exception");
+                System.err.println("inner try block 1");
+            }
+        }
+
+        //Handle array index out of bounds 
+        catch(ArrayIndexOutOfBoundsException e4) 
+        {
+            System.out.println(e4);
+            System.out.println("outer main try block");
+        }
+        catch (Exception e5)
+        {
+            System.out.println("Exception");
+            System.out.println("Handled in main block");
+        }
     }
 }
