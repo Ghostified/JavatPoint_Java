034fe819833414809dd44f40e6f4aec88c74a7ca
Allan Branson <bransonaallan@gmail.com>
2024-03-13 21:56:44 +0300

    Throw and throws example

--- ExceptionHandling/ThrowsKeyword.java
+++ ExceptionHandling/ThrowsKeyword.java
@@ -2,6 +2,8 @@ package ExceptionHandling;
 
 import java.io.IOException;
 
+import Enscapulation.Example2.TestAccount;
+
 /*
  * The thows keyword id used to declare an exception
  * it informs the programmer that the exception might occur
@@ -12,6 +14,10 @@ public static void main(String[] args) {
     obj.p();
     
     M.method2();
+
+    TestThrow3.CheckNum(-3);
+
+    TestThrows4.catchMethod();
 
 }
 }
@@ -54,3 +60,43 @@ class M {
     }
 }
 
+//throw example 
+class TestThrow3 {
+    public static void CheckNum (int num ) {
+        try {
+        if (num < 1) {
+            throw new ArithmeticException("\n Number is negative , can not calculate square");
+        }
+        else {
+            System.out.println("Sqaure of num " + num + " is " + (num * num));
+        }
+    }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("rest of the code ");
+        }
+    
+    }
+}
+
+//throws example 
+class TestThrows4 {
+    public static int divideNum (int m, int n) throws ArithmeticException {
+        int div = m/n;
+        return div;
+    }
+
+ static void catchMethod () {
+    try {
+        System.out.println(divideNum(45,0));
+    }
+    catch (ArithmeticException e) {
+        System.out.println("\n number can not be divided by zero");
+    }
+    finally {
+        System.out.println("Rest of  the code ");
+    }
+ }
+}
