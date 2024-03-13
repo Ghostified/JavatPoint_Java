b904902c9b1fba1182fd9d1ea317c172dba1cbcf
Allan Branson <bransonaallan@gmail.com>
2024-03-13 19:29:27 +0300

    checked exceptions are not propagated

--- ExceptionHandling/ExceptionPropagation.java
+++ ExceptionHandling/ExceptionPropagation.java
@@ -1,9 +1,14 @@
 package ExceptionHandling;
 
+import java.io.IOException;
+
 public class ExceptionPropagation {
     public static void main(String[] args) {
         Example28.p();
         System.out.println("normal flow");
+
+        Example29 obj = new Example29();
+        obj.p();
     }
 
 }
@@ -27,4 +32,25 @@ class Example28 {
             System.out.println("Exception handled");
         }
     }
+}
+
+class Example29 {
+     void m () {
+        try {
+            throw new java.io.IOException("device error");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+    }
+    void n () {
+        m();
+    }
+    void p() {
+        try {
+            n ();
+        }
+        catch (Exception e) {System.out.println("exception handled");}
+    }
+
 }
