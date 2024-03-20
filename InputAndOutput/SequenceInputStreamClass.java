337a262cf81b01ab854134f4bfdee1ab6dab1784
Allan Branson <bransonaallan@gmail.com>
2024-03-20 15:51:24 +0300

    the sequenceInputStream class can be used to read two files and write int another file

--- InputAndOutput/SequenceInputStreamClass.java
+++ InputAndOutput/SequenceInputStreamClass.java
@@ -1,6 +1,7 @@
 package InputAndOutput;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.SequenceInputStream;
 
 /*
@@ -9,6 +10,8 @@ import java.io.SequenceInputStream;
 public class SequenceInputStreamClass {
  public static void main(String[] args) throws Exception {
     Example1.inputStreamExample();
+
+    ReadingTwofiles.concurrentReadAndWrite();
  }
 }
  class Example1 {
@@ -28,4 +31,22 @@ public class SequenceInputStreamClass {
  }
 
  //Example that reads the data fromn two files and writes in another file 
- 
+ class ReadingTwofiles {
+    static void concurrentReadAndWrite () throws Exception {
+        FileInputStream file1 = new FileInputStream("C:\\Users\\allan.branson\\Downloads\\testout.txt");
+        FileInputStream file2 = new FileInputStream("C:\\Users\\allan.branson\\Downloads\\testout2.txt");
+        FileOutputStream fout = new FileOutputStream("C:\\Users\\allan.branson\\Downloads\\testinput.txt");
+
+        SequenceInputStream sis = new SequenceInputStream( file1,file2);
+
+        int i ;
+        while ((i = sis.read())!= -1) {
+            fout.write(i);
+        }
+        sis.close();
+        fout.close();
+        file1.close();
+        file2.close();
+        System.out.println("Success");
+    }
+ }
