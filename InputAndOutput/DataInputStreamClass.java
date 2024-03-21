08285d06a0834caee1b3bbfb3ac2571eeaba1bcb
Allan Branson <bransonaallan@gmail.com>
2024-03-21 23:01:12 +0300

    All methods implementation of the DataInputStream Class implemetation

---
+++ InputAndOutput/DataInputStreamClass.java
@@ -0,0 +1,61 @@
+package InputAndOutput;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.sound.midi.Soundbank;
+
+/*
+ * DataInputStreamClass  allows an application to read primitive data types
+ */
+public class DataInputStreamClass {
+ public static void main(String[] args) {
+    dataInputStreamMethods();
+    
+ }
+
+ public static void dataInputStreamMethods () {
+    try {
+        FileInputStream fis = new FileInputStream("C:\\Users\\allan.branson\\Downloads\\testout.txt");
+
+        DataInputStream dis = new DataInputStream(fis);
+        
+        //Read data using various methods of Data Input Stream
+        String stringValue = dis.readUTF();
+        byte readByte = dis.readByte();
+        short shortValue = dis.readShort();
+         int intValue = dis.readInt();
+         long longValue = dis.readLong();
+        char character = dis.readChar();
+        float floatValue = dis.readFloat();
+        double doubles = dis.readDouble();
+
+        //Read a string as a byte sequence
+        //byte [] bytes  = new byte[10];
+       // dis.read(bytes);
+       // String stringValue = new String(bytes);
+
+        //Read a string using UTF-8 encoding
+       // String utfStringValue = dis.readUTF();
+
+        //print the read data
+        System.out.println("Byte value: " + readByte);
+        System.out.println("Short value : " + shortValue);
+        System.out.println("int value : " + intValue);
+        System.out.println("Long Value: " +longValue);
+        System.out.println("Character Value " + character);
+        System.out.println("Float Vlaue:  " + floatValue);
+        System.out.println("Double Value: " + doubles);
+        System.out.println("String value " + stringValue.trim());
+        System.out.println("UTF -8 string: " + stringValue);
+
+        //Close DataInputStream
+        dis.close();
+
+    } catch (IOException   e) {
+        e.printStackTrace();
+    }
+ }
+}
