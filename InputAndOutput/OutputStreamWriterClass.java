abcdfcfe741c9b652c8d07abbc8517c0ed418ec2
Allan Branson <bransonaallan@gmail.com>
2024-03-26 23:56:24 +0300

    OutPutStreamWriter methods

--- InputAndOutput/OutputStreamWriterClass.java
+++ InputAndOutput/OutputStreamWriterClass.java
@@ -1,6 +1,8 @@
 package InputAndOutput;
 
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -12,6 +14,7 @@ public class OutputStreamWriterClass {
 
     public static void main(String[] args) {
        outputStreamWriterClass(); 
+       outputStreamWriterMethods();
     }
 
     public static void outputStreamWriterClass () {
@@ -23,6 +26,38 @@ public class OutputStreamWriterClass {
             outputStreamWriter.write("what");
             outputStreamWriter.close();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void outputStreamWriterMethods () {
+        String filepath = "C:\\Users\\allan.branson\\Downloads\\testout.txt";
+
+        try (OutputStream outputStream = new FileOutputStream(filepath);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream))) {
+           
+            
+        //Creating an outputStream using default character encoding
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+
+        //writing characters to the file using the write () method
+        outputStreamWriter.write("Hello , Hogwarts");
+
+        //writing a portion of an array characters using the write (char [] int int method)
+        char [] charArray = {'a','b','c','d','e','f'};
+        outputStreamWriter.write(charArray, 0,3); //writting only the first three chars
+
+        //wring a single char using write (int) method
+        outputStreamWriter.write(' ');
+
+        //writing a portion of the string using the write(String , int , int) method
+        String string = "Wingardium Leviosa";
+        outputStreamWriter.write(string, 0, 12); //writing only the first 12 characters
+
+        //closing the stream
+        outputStreamWriter.close();
+
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
