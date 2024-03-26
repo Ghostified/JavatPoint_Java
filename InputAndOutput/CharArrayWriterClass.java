2b0f83f81c4ff5cd3fcb54884ddd65158a30bc28
Allan Branson <bransonaallan@gmail.com>
2024-03-26 16:15:33 +0300

    CharArrayWriter Methods

--- InputAndOutput/CharArrayWriterClass.java
+++ InputAndOutput/CharArrayWriterClass.java
@@ -5,6 +5,9 @@ import java.io.CharArrayWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+
+import javax.sound.midi.Soundbank;
 
 /*
  * This class can be used to writecommon data to multiple files ,i.e one to many
@@ -13,6 +16,7 @@ public class CharArrayWriterClass {
 
     public static void main(String[] args) {
         charWriter();
+        charArrayWriterMethods();
         
     }
     public static void charWriter () {
@@ -34,7 +38,7 @@ public class CharArrayWriterClass {
             f3.close();
             f4.close();
 
-            FileReader file = new FileReader("C:\\Users\\allan.branson\\Downloads\\testout.txt");
+            FileReader file = new FileReader("C:\\Users\\allan.branson\\Downloads\\TestOut2.txt");
             BufferedReader object = new BufferedReader(file);
 
 
@@ -44,6 +48,43 @@ public class CharArrayWriterClass {
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
 
+    public static void charArrayWriterMethods () {
+        try {
+
+            //Create a charArraywriter object
+            CharArrayWriter writer = new CharArrayWriter();
+
+            //write characters to the buffer 
+            writer.write('H');
+            writer.write("ello", 0, 3);
+
+            //get the size of the buffer 
+            int size = writer.size ();
+            System.out.println("Size of the Buffer: " +size);
+
+            //Convert the buffer to a char array and string
+            char [] charArray = writer.toCharArray();
+            String str = writer.toString();
+
+            System.out.println("Content as a char array:" + new String(charArray));
+            System.out.println("Content as string: " + str);
+
+            //write the content to another writer
+            Writer out = new java.io.FileWriter("C:\\Users\\allan.branson\\Downloads\\TestOut2.txt");
+            writer.writeTo(out);
+            out.close();
+
+            //reset the buffer 
+            writer.reset();
+
+            //flush and close the writer
+            writer.flush();
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
