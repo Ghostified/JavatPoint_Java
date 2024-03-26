7ab41beff2f58c47c5e6bd7e70ce2ab3f9d3eff1
Allan Branson <bransonaallan@gmail.com>
2024-03-26 23:10:05 +0300

    PrintWriter Methods

--- InputAndOutput/PrintWriterClass.java
+++ InputAndOutput/PrintWriterClass.java
@@ -3,6 +3,7 @@ package InputAndOutput;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,7 +15,7 @@ public class PrintWriterClass {
 
     public static void main(String[] args) {
         printWriter1();
-        
+        printWriterExample2();
     }
 
     //example of writing the data on a console and in a text file using the PrintWriter class methods
@@ -39,6 +40,41 @@ public class PrintWriterClass {
             for (int counter = 0; counter < 3; counter++) {
                 System.out.println(bf.readLine());
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printWriterExample2 () {
+
+        String filePath = "C:\\Users\\allan.branson\\Downloads\\testout.txt";
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+
+            //writing diffrent types of data in the file 
+            writer.println("Hi Harry");
+            writer.println(123);
+            writer.println(true);
+
+            //Appending characters aand character sequences
+            writer.append('a');
+            writer.append("ABD");
+            writer.append("EFG", 1, 2); //Appends a subsequence of "EFG " from index 1 and 2
+
+            //checking error state
+            if (writer.checkError()) {
+                System.out.println("Error occured while writing to file");
+
+            }  else {
+                System.out.println("Success");
+            }
+
+            //Flush the stream
+            writer.flush();
+
+            //Closing the stream
+            writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
