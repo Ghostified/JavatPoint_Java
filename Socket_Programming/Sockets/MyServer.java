01526f890ee2459e1b7e1bba2d61fe1b784af1b3
Allan Branson <bransonaallan@gmail.com>
2024-04-07 23:36:37 +0300

    URL Class

--- Socket_Programming/Sockets/MyServer.java
+++ Socket_Programming/Sockets/MyServer.java
@@ -10,7 +10,9 @@ import java.net.Socket;
  */
 public class MyServer {
     public static void main(String[] args) {
+        
         try {
+
             ServerSocket ss = new ServerSocket(6666);
             Socket s = ss.accept(); //tablishes a connection
             DataInputStream dis = new DataInputStream(s.getInputStream());
