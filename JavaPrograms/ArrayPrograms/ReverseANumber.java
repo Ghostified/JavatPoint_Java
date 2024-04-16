9fca048479cf930de0b80cd449f75c9c83e28733
Allan Branson <bransonaallan@gmail.com>
2024-04-16 19:22:22 +0300

    reverse a number

--- JavaPrograms/ArrayPrograms/ReverseANumber.java
+++ JavaPrograms/ArrayPrograms/ReverseANumber.java
@@ -4,6 +4,8 @@ public class ReverseANumber {
 
     public static void main(String[] args) {
         example1();
+        example2();
+        usingForLoops();
     }
 
     public static void example1 () {
@@ -20,6 +22,31 @@ public class ReverseANumber {
     }
 
     public static void example2 () {
-        
+        int number = 1234;
+        int reverse = 0;
+   
+        while (number != 0) {
+            int remainder   = number % 10;
+            reverse =  reverse * 10 + remainder;
+            number = number/ 10;
+
+        }
+        System.out.println(reverse);
+    }
+
+    //using a for loop 
+
+    public static void usingForLoops () {
+        int number = 2345;
+        int reverse = 0;
+     for (;number != 0; ) {
+
+        int remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number /10;
+     }
+
+     System.out.println(reverse);
+
     }
 }
