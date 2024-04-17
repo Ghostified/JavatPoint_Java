859ce969c4ee50d82d718803c0ac2646a31b08fd
Allan Branson <bransonaallan@gmail.com>
2024-04-18 01:31:00 +0300

    JtextArea with Action Event Listener

--- Swing/JTextAreaClass.java
+++ Swing/JTextAreaClass.java
@@ -1,11 +1,16 @@
 package Swing;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 public class JTextAreaClass {
     public static void main(String[] args) {
-       jTextAreaExample(); 
+       //jTextAreaExample(); 
+       new TextAreaExample();
     }
 
     public static void jTextAreaExample () {
@@ -21,3 +26,37 @@ public class JTextAreaClass {
 
 }
 //JtextArea with an event listener
+
+class TextAreaExample  implements ActionListener {
+    JLabel l1, l2;
+    JTextArea area;
+    JButton b ;
+    TextAreaExample () {
+
+        JFrame f = new JFrame();
+        l1 = new JLabel();
+        l1.setBounds(50,25,100,30);
+        l2 = new JLabel();
+        l2.setBounds(160,25,100,30);
+        area = new JTextArea();
+        area.setBounds(20,75,250,200);
+        b = new JButton("Count Words");
+        b.setBounds(100,300,120,30);
+        b.addActionListener((ActionListener) this);
+        f.add(b);
+        f.add(l1);
+        f.add(l2);
+        f.add(area);
+        f.setSize(450,450);
+        f.setLayout(null);
+        f.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String text = area.getText();
+        String words [] = text.split("\\s");
+        l1.setText("words: " + words.length);
+        l2.setText("Characters: " + text.length());
+    }
+}
