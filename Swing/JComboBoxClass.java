d6056fa41e96de57ce79ea5c144565e1fa95d28c
Allan Branson <bransonaallan@gmail.com>
2024-04-24 23:03:15 +0300

    JComboBox with Action Listener

--- Swing/JComboBoxClass.java
+++ Swing/JComboBoxClass.java
@@ -1,5 +1,6 @@
 package Swing;
 import javax.swing.*;
+import java.awt.event.*;
 /*
  * Used to create a pop up menu with choices
  */
@@ -7,7 +8,8 @@ public class JComboBoxClass {
 
     public static void main (String [] Args) {
 
-        comboBoxExample();
+        //comboBoxExample();
+        JComboBoxExample2.jComboBoxWithActionListener();
 
     }
 
@@ -20,5 +22,34 @@ public class JComboBoxClass {
         f.setLayout(null);
         f.setSize(400,400);
         f.setVisible(true);
+    }
+}
+
+//JComboBox with Action Listener
+class JComboBoxExample2 {
+    public static void jComboBoxWithActionListener () {
+
+        JFrame f = new JFrame("ComboBox Example ");
+        JLabel jl = new JLabel();
+        jl.setHorizontalAlignment(JLabel.CENTER);
+        jl.setSize(400,100);
+        JButton jb = new  JButton("Show");
+        jb.setBounds(200,100,75,20);
+        String [] languages = {"PHP", "JAVA", "PYTHON", "Flutter","Koylin"};
+        final JComboBox cb = new JComboBox<>(languages);
+        cb.setBounds(50,100,90,20);
+        f.add(cb);
+        f.add(jb);
+        f.add(jl);
+        f.setLayout(null);
+        f.setSize(350,350);
+        f.setVisible(true);
+        jb.addActionListener(new ActionListener  (){
+            public void actionPerformed(ActionEvent e) {
+                String data = "Programming Language Selected: "
+            + cb.getItemAt(cb.getSelectedIndex());
+            jl.setText(data);
+            }
+        });
     }
 }
