bd42290b798038f17bf257db8769ce40044f974b
Allan Branson <bransonaallan@gmail.com>
2024-04-25 01:48:07 +0300

    find the third largest using collections

--- JavaPrograms/ArrayPrograms/FindThirdLargestElementInArray.java
+++ JavaPrograms/ArrayPrograms/FindThirdLargestElementInArray.java
@@ -2,13 +2,20 @@ package JavaPrograms.ArrayPrograms;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.sound.midi.Soundbank;
 
 public class FindThirdLargestElementInArray {
     public static void main(String[] args) {
 
         int x [] = {91,34,56,77,88,99};
+        Integer y [] = {30,55,66,77,88,9921,345,6789};
         example1();
         System.out.println(sortArray(x, 6));
+        System.out.println(getThirdWithCollections(y, 8));
         
     }
 
@@ -40,6 +47,14 @@ public class FindThirdLargestElementInArray {
         return b[total -3];
     }
 
+    //get the third largest element in Array using collections
+    public static int getThirdWithCollections (Integer []a , int total) {
+
+        List <Integer> list = Arrays.asList(a);
+        Collections.sort(list);
+        int element = list.get(total -3);
+        return element;
+    }
 }
 
 
