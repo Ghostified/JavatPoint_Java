d3b579a2dfd615352e16cab1fc4d01fb3e1e533b
Allan Branson <bransonaallan@gmail.com>
2024-05-01 16:33:25 +0300

    Jlist with action Listener

--- Swing/JavaJList.java
+++ Swing/JavaJList.java
@@ -1,12 +1,18 @@
 package Swing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 public class JavaJList {
     public static void main(String[] args) {
-        jlistExample1();
+        //jlistExample1();
+        jlistWithActionListener();
         
     }
 
@@ -23,4 +29,54 @@ public class JavaJList {
         f.setLayout(null);
         f.setVisible(true);
     }
+
+    //JList with action Listener
+
+    public static void jlistWithActionListener () {
+        JFrame f = new JFrame();
+        final JLabel label = new JLabel();
+        label.setSize(500, 100);
+        JButton b = new JButton("Show");
+        b.setBounds(200, 150, 80, 30);
+        final DefaultListModel <String> l1 = new DefaultListModel<>();
+        l1.addElement("C");
+        l1.addElement("C++");
+        l1.addElement("Java");
+        l1.addElement("PHP");
+        final JList <String> list1 = new JList<>(l1);
+        list1.setBounds(100,100,75,75);
+        DefaultListModel <String> l2 = new DefaultListModel<>();
+        l2.addElement("Turbo C++");
+        l2.addElement("Struts");
+        l2.addElement("Spring");
+        l2.addElement("YII");
+        //l2.addElement("Pascal");
+        final JList <String> list2 = new JList<>(l2);
+        list2.setBounds(100, 200, 75, 75);
+        f.add(list1);
+        f.add(list2);
+        f.add(b);
+        f.add(label);
+        f.setSize(450, 450);
+        f.setLayout(null);
+        f.setVisible(true);
+        b.addActionListener(new ActionListener () {
+            @SuppressWarnings("deprecation")
+            public void actionPerformed(ActionEvent e) {
+                String data = "";
+                if (list1.getSelectedIndex() != -1){
+                    data = "Programming language Selected" + list1.getSelectedValue ();
+                    label.setText(data);
+                }
+                if (list2.getSelectedIndex() != -1){
+                    data += " . Framework Selected: " ;
+                    for (Object frame : list2.getSelectedValues()) {
+                        data += frame + " ";
+                    }
+                }
+                label.setText(data);
+            }
+        });
+    }
+
 }
