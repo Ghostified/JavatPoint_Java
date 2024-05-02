d069608fafc6cddb45d7b33e7e2df95eeead690a
Allan Branson <bransonaallan@gmail.com>
2024-05-03 01:09:14 +0300

    adding an adjustment listener to the scroll bar

--- Swing/ScrollBar.java
+++ Swing/ScrollBar.java
@@ -1,7 +1,9 @@
 package Swing;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollBar;
+import java.awt.event.*;
 
 /*
  * Used to add a vertical or Horizontal Scroll Bar 
@@ -10,6 +12,7 @@ public class ScrollBar {
 
     public static void main(String[] args) {
        scrollBar(); 
+       scrollBarExample2();
     }
 
     //Scroll Bar Example 
@@ -21,6 +24,27 @@ public class ScrollBar {
         f.setSize(400,400);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    //Scrollbar with adjustment Listener
+    public static void scrollBarExample2 () {
+        JFrame f = new JFrame("Scoll bar with Adjustments");
+        final JLabel b = new JLabel();
+        b.setHorizontalAlignment(JLabel.CENTER);
+        b.setSize(400,100);
+        final JScrollBar s = new JScrollBar();
+        s.setBounds(100,100,50,100);
+        f.add(s);
+        f.add(b);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        s.addAdjustmentListener(new AdjustmentListener(){
+            public void adjustmentValueChanged(AdjustmentEvent e){
+                b.setText("Vertical ScrollBar Value is: " + s.getValue());
+            }
+        });
+
     }
 }
 
