a6f0e9f4f1a502aa2e396d53888dbc1b190ff29c
Allan Branson <bransonaallan@gmail.com>
2024-05-02 17:47:14 +0300

    JOption Pane with a window adapter and listener

--- Swing/JavaJOptionPane.java
+++ Swing/JavaJOptionPane.java
@@ -41,27 +41,4 @@ public class JavaJOptionPane{
 
 }
 
- class ShowConfirmDialog1 extends WindowAdapter {
 
-    //JOption Pane showConfirmDialog () 
-    //Used to create dialog with option yes, No, and cancel. Tith title -> Select an Option
-  
-    private static JFrame f;
-
-    public static void jOptionPaneExample4 () {
-        f = new JFrame();
-        f.addWindowListener(new ShowConfirmDialog1 ());
-        f.setSize(300, 300);
-        f.setLayout(null);
-        f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        f.setVisible(true); 
-    }
-
-    public void windowClosing(WindowEvent e) {
-        int a = JOptionPane.showConfirmDialog(f,"Are you sure? ");
-        if (a == JOptionPane.YES_OPTION){
-            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        }
-    }
-
-}
