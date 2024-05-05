1de8568e25cc95f213d6f0bf154fc334019afe18
Allan Branson <bransonaallan@gmail.com>
2024-05-05 22:42:23 +0300

    color chooser with action listener

--- Swing/ColorChooser.java
+++ Swing/ColorChooser.java
@@ -4,6 +4,8 @@ import javax.accessibility.Accessible;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -15,6 +17,9 @@ public class ColorChooser {
         ColorChooserExample ch = new ColorChooserExample();
         ch.setSize(400,400);
         ch.setVisible(true);
+
+        //Example two
+        new ColorChooserExample2();
         
     }
 }
@@ -35,5 +40,30 @@ class ColorChooserExample extends JFrame implements ActionListener {
         Color initialColor = Color.RED;
         Color color = JColorChooser.showDialog(this, "Select a color", initialColor);
         c.setBackground(color);
+    }
+}
+
+//Color chooser with ActionListener Example
+
+class ColorChooserExample2 extends JFrame implements ActionListener {
+    JFrame f;
+    JButton b;
+    JTextArea ta;
+    ColorChooserExample2 () {
+        f = new JFrame("Color Chooser Example with Action Listener");
+        b = new JButton("Pad Color");
+        b.setBounds(200,250,100,30);
+        ta = new JTextArea();
+        ta.setBounds(10,10,300,200);
+        b.addActionListener(this);
+        f.add(b);
+        f.add(ta);
+        f.setLayout(null);
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e) {
+        Color c = JColorChooser.showDialog(this, "Choose", Color.CYAN);
+        ta.setBackground(c);
     }
 }
