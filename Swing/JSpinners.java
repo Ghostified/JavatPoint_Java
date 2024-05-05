045adfc14b1f9b72c06059b2a2f424327cd8bf5d
Allan Branson <bransonaallan@gmail.com>
2024-05-05 23:29:40 +0300

    Create a JSpinner with a change Listener

--- Swing/JSpinners.java
+++ Swing/JSpinners.java
@@ -1,9 +1,12 @@
 package Swing;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /*
  * JSpinner class is asingle object used to select a number of Objects from an ordered sequence
@@ -11,7 +14,8 @@ import javax.swing.SpinnerNumberModel;
 public class JSpinners {
 
     public static void main(String[] args) {
-        new SpinnerExample();
+       // new SpinnerExample();
+       new JSpinnerWithChangeListener();
         
     }
 }
@@ -32,3 +36,32 @@ public class JSpinners {
         f.setVisible(true);
     }
  }
+//JSpinner with ChangeListener  example
+class JSpinnerWithChangeListener {
+    JSpinnerWithChangeListener () {
+        JFrame f = new JFrame("Spinner Example with Change Listener");
+        final JLabel label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setSize(250, 100);
+
+        SpinnerModel value = 
+            new SpinnerNumberModel(
+                0, //initial value
+                0, //Minimum value
+                50, //Maximum value
+                10 //Steps
+            );
+        JSpinner spinner = new JSpinner(value);
+        spinner.setBounds(100,100,50,30);
+        f.add(spinner);
+        f.add(label);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+        spinner.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                label.setText("Value: " + ((JSpinner) e.getSource()).getValue());
+            }
+        }); 
+    }
+}
