de3f78e0174b0f0b3bafaab277dca694eac59046
Allan Branson <bransonaallan@gmail.com>
2024-05-07 06:46:27 +0300

    Creating  JTOggleButton with an Item Listener

--- Swing/JToggleButtons.java
+++ Swing/JToggleButtons.java
@@ -1,12 +1,21 @@
 package Swing;
 
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.JFrame;
+import javax.swing.JToggleButton;
 
 //Its used to create a toggle button , like on/off
 public class JToggleButtons  extends JFrame implements ItemListener{
 
     public static void main(String[] args) {
-        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run () {
+                new JToggleButtons();
+            }
+        });
     }
 
     private JToggleButton button ;
@@ -18,5 +27,25 @@ public class JToggleButtons  extends JFrame implements ItemListener{
         setSize (200,200);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void setJToggleButton () {
+
+        //Initialize the toggle Button  with the Text "ON"
+        button = new JToggleButton("ON");
+
+        //Add the Toggle Button to the Frame
+        add(button);
+    }
+
+    private void setAction () {
+        button.addItemListener(this);
+    }
+
+    public void itemStateChanged(ItemEvent eve) {
+        if (button.isSelected())
+            button.setText("OFF");
+        else 
+            button.setText("ON");
     }
 }
