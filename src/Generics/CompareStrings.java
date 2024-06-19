209d172406a2919fb4a9c2650a1cd5c3448e4c96
Ghostified <bransonallan@gmail.com>
2024-06-19 23:50:52 +0300

    string compare

---
+++ src/Generics/CompareStrings.java
@@ -0,0 +1,16 @@
+package Generics;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+
+        String website = "a9d202e74f67244c81fc78e8327388725d88ae3ac72deb243b49d836090fe054";
+
+        String kati = "a9d202e74f67244c81fc78e8327388725d88ae3ac72deb243b49d836090fe054";
+
+        if ( website == kati){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
+}
