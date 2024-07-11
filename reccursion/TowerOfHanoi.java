e9a0dc54e9f6761ec8c408760ffa5486a411bbdf
Ghostified <bransonallan@gmail.com>
2024-07-11 23:56:49 +0300

    towers of hanoi

---
+++ reccursion/TowerOfHanoi.java
@@ -0,0 +1,21 @@
+package reccursion;
+
+public class TowerOfHanoi {
+    
+    // Recursive function to solve Tower of Hanoi
+    public static void solveHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+        solveHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod);
+        solveHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        int n = 3; // Number of disks
+        solveHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
+    }
+}
